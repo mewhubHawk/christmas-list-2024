57d3e7def7c953c1f2536667cb1d224e309094d0
Kriemhild Gretchen <34939082+mewhubHawk@users.noreply.github.com>
2024-11-26 21:32:45 +0000

    upload files via upload
    copy and edit basic details from christmas-list-2023
    make plans for code change:
    #would like to make it so seconds and hours are rounded up always 
    #28days 5 hours to go is then 29 days

---
+++ main (7).py
@@ -0,0 +1,36 @@
+import time
+import datetime
+
+delay=1.0  
+
+def FormatedList(title, presents_file, delay):
+  with open(presents_file) as file:
+    presents_list = file.readlines()
+
+  print(title)
+  time.sleep(0.4)
+  for item in presents_list:
+    print(f'--{item.strip()}') 
+    time.sleep(delay)
+  print(" ")
+
+print("My Christmas List \n")
+
+now = time.time()
+time.sleep(0.4)
+christmas = time.strptime("25 dec 24", "%d %b %y")
+how_long_to_wait = time.mktime(christmas) - now
+#would like to make it so seconds and hours are rounded up always 
+#28days 5 hours to go is then 29 days
+
+print(f'Today is {time.asctime()}.')
+time.sleep(0.4)
+
+print(f'christmas is {time.asctime(christmas)}')
+time.sleep(0.5)
+
+print("That's {} to go... \n".format(datetime.timedelta(seconds=how_long_to_wait)))
+time.sleep(0.6)
+
+FormatedList("1. BOOKS:", "books.txt", delay)
+FormatedList("2. CLOTHING:", "clothing.txt", delay)
