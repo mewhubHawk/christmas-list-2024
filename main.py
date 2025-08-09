41ee9111ace3312e92c9f631911cf4114058c343
Kriemhild Gretchen <34939082+mewhubHawk@users.noreply.github.com>
2025-08-09 19:20:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ print("\nMy Christmas List \n")
 
 now = time.time()
 time.sleep(0.4)
-christmas = time.strptime("25 dec 24", "%d %b %y")
+christmas = time.strptime("25 dec {input}", "%d %b %y")
 #christmas = time.strptime("25 dec",(input), "%d %b %y")
 #ability to select the year
 how_long_to_wait = time.mktime(christmas) - now
@@ -35,6 +35,7 @@ time.sleep(0.5)
 print("That's {} to go... \n".format(datetime.timedelta(seconds=how_long_to_wait)))
 time.sleep(0.6)
 
+FormatedList("PREFACE:", "preface.txt", delay)
 FormatedList("1. BOOKS:", "books.txt", delay)
 FormatedList("2. CLOTHING:", "clothing.txt", delay)
 FormatedList("3. DIY(WD(with Dad)):", "diy.txt", delay)
